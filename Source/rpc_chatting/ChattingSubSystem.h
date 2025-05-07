dfda9faf0c0fcb66c1dcd57e4423b80593db0e7e
Ken9903 <godsdy0410@naver.com>
2025-05-07 22:19:22 +0900

    실행 체크

---
+++ Source/rpc_chatting/ChattingSubSystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ChattingSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPC_CHATTING_API UChattingSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
